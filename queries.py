13c73e866c21a34450029e4498c0136b75c959c3
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-12 14:36:23 +0200

    Corrected code to include SQL string comparison for 'love'

--- queries.py
+++ queries.py
@@ -19,7 +19,16 @@ def directors_list(db):
 def love_movies(db):
     # return the list of all movies which contain the exact word "love"
     # in their title, sorted in alphabetical order
-    query = "select m.title  from movies m WHERE UPPER(m.title) LIKE '%LOVE%'"
+    query = "select m.title  from movies m \
+        WHERE UPPER(m.title) LIKE '% LOVE %'\
+        OR UPPER(m.title) LIKE '% LOVE' \
+        OR UPPER(m.title) LIKE 'LOVE' \
+        OR UPPER(m.title) LIKE 'LOVE &%' \
+        OR UPPER(m.title) LIKE 'LOVE %' \
+        OR UPPER(m.title) LIKE '%LOVE, %' \
+        OR UPPER(m.title) LIKE '%LOVE.' \
+        OR UPPER(m.title) LIKE '%LOVE''%' \
+        ORDER BY m.title ASC"
     db.execute(query)
     results = db.fetchall()
     return [each_movie[0] for each_movie in results]
@@ -28,8 +37,8 @@ def love_movies(db):
 def directors_named_like_count(db, name):
     # return the number of directors which contain a given word in their name
     query = f"SELECT COUNT(d.name) \
-    FROM directors d \
-    WHERE UPPER(d.name) LIKE '%{name}%'"
+        FROM directors d \
+        WHERE UPPER(d.name) LIKE '%{name}%'"
     db.execute(query)
     result = db.fetchone()
     return result[0]
